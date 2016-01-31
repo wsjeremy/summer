c0a581d15583ce1489b703fcd923d3f28e026d49
wsjeremy <wsjeremyxjp@gmail.com>
2016-01-31 22:44:42 +1100

    01/31

---
+++ summer1/unique.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Jeremy on 31/01/2016.
+ */
+/**
+ * Created by Jeremy on 17/12/2015.
+ */
+
+function unique(array){
+    var n = []; //一个新的临时数组
+    var u = [];
+    //遍历当前数组
+
+    for(var i = 0; i < array.length; i++){
+        //如果当前数组的第i已经保存进了临时数组，那么跳过，
+        //否则把当前项push到临时数组里面
+        if (n.indexOf(array[i]) == -1)
+            n.push(array[i].trim());
+        //trim去除空格
+    }
+
+    return n;
+
+}
+
+
+
+
+
+
