56acdac9c08bb7a8aea18aacba2b5f9dcd3dbb37
wsjeremy <wsjeremyxjp@gmail.com>
2016-02-06 10:41:44 +1100

    02/06
    修改后

---
+++ summer6号/00/00X/0000n/deleteduplicate00.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Jeremy on 5/02/2016.
+ */
---
+++ summer6号/output.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Jeremy on 5/02/2016.
+ */
+function output(array){
+    var n=[];
+    for(i=0;i<array.length;i++){
+        n.push(array[i]);
+        if (i%4==0){
+
+
+        }
+
+    }
+
+
+
+}
