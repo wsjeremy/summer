fecefcd84d70c87932bb789ab7c33204837be018
wsjeremy <wsjeremyxjp@gmail.com>
2016-02-08 13:33:24 +1100

    02/08
    跳转还有问题， 第一页 改成了tooltip

---
+++ summer02:08/outputString.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Jeremy on 8/02/2016.
+ */
+function output(string){
+    var n=[];
+    var result="";
+    for(i=0;i<string.length;i+=7){
+        for (j=i;j<string.length&&j<i+7;j++){
+            n.push(string[j]);
+            if (result===""){result=string[j];}
+            else{result=result+string[j];}
+        }
+        result=result+"<br>";
+    }
+
+    return result;
+
+}
